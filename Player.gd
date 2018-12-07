d0f2ed4503d3529db2df08202d457fb55a05a15c
fluxrider <doompilot@gmail.com>
2018-12-07 12:52:02 -0500

    tinker with kart controls

--- Player.gd
+++ Player.gd
@@ -1,38 +1,45 @@
 extends KinematicBody
 
-var speed = 20 # meters per seconds
-var jump_power = 100 # meters per seconds, injected as dirac action
+var speed = 1 # meters per seconds
+var turn_speed = 3 # angle per seconds (rad)
+var jump_power = 50 # meters per seconds, injected as dirac action
 var velocity = Vector3()
+var facing = 0 # bird eye view angle the kart's pedal accelerates towards
+var acceleration = Vector3()
+var accel_cap = 20
+var accel_decay = 4
 
 export(NodePath) var camera
 
 func _physics_process(delta):
-	# read joystick axis
-	var d = Vector2()
-	d.x = Input.get_joy_axis(0, 0)
-	d.y = Input.get_joy_axis(0, 1)
-	# axis deadzone
-	if abs(d.x) < 0.05: d.x = 0
-	if abs(d.y) < 0.05: d.y = 0
-	# read keyboard
 	var key_fw = Input.get_action_strength("move_forward")
 	var key_bw = Input.get_action_strength("move_backward")
 	var key_le = Input.get_action_strength("move_left")
 	var key_ri = Input.get_action_strength("move_right")
-	if key_fw != 0 and -key_fw < d.y: d.y = -key_fw
-	if key_bw != 0 and key_bw > d.y: d.y = key_bw
-	if key_le != 0 and -key_le < d.x: d.x = -key_le
-	if key_ri != 0 and key_ri > d.x: d.x = key_ri
 
-	# direction is relative to camera, not absolute axis
-	d = d.rotated(-get_node(camera).azymuth - PI/2)
+	# rotate
+	facing += (key_ri - key_le) * -turn_speed * delta
+
+	# acceleration
+	var d = Vector2()
+	d.x = key_fw - key_bw
+	d.y = 0
+	d = d.rotated(-facing - PI/2)
+	acceleration.x += d.x
+	acceleration.z += d.y
 
 	# apply speed and gravity
-	velocity.x = d.x * speed
-	velocity.z = d.y * speed
+	velocity.x = acceleration.x * speed
+	velocity.z = acceleration.z * speed
 	velocity.y += -9.8
 	# note: delta is applied magically inside move_and_slide, see doc.
 	velocity = self.move_and_slide(velocity, Vector3(0,1,0))
+
+	# friction (cap)
+	if acceleration.length() > accel_cap:
+		acceleration = acceleration.normalized() * accel_cap
+	# friction (decay)
+	acceleration -= (acceleration * accel_decay * delta)
 
 	# jump action (next frame)
 	if self.is_on_floor() and Input.is_action_just_pressed("ui_accept"):
