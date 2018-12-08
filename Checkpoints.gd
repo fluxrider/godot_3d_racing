0284bebf355868da92ebf79df7494165b54ed9c9
fluxrider <doompilot@gmail.com>
2018-12-08 16:45:05 -0500

    lap time

--- Checkpoints.gd
+++ Checkpoints.gd
@@ -10,8 +10,10 @@ func _ready():
 	$Area3.connect("body_entered", self, "on_body_entered", [3])
 	self.connect("lap", self, "on_lap")
 
-func on_lap(user, lap):
+func on_lap(user, lap, time):
 	print(str(user) + " is on lap " + str(lap))
+	if lap > 1:
+		print(time)
 
 func on_body_entered(body, id):
 	var user = body.name
@@ -21,7 +23,8 @@ func on_body_entered(body, id):
 			laps[user] = {
 				lap = 0,
 				prev = 3,
-				checkpoint = 0
+				checkpoint = 0,
+				time = 0
 			}
 		# update lap data for this user
 		var info = laps[user]
@@ -29,7 +32,9 @@ func on_body_entered(body, id):
 			if info.prev == 3 and (info.lap == 0 or info.checkpoint == 3):
 				info.lap += 1
 				info.checkpoint = 1
-				emit_signal("lap", user, info.lap)
+				var now = OS.get_ticks_msec()
+				emit_signal("lap", user, info.lap, now - info.time)
+				info.time = now
 		else:
 			if info.checkpoint == id - 1 and info.prev == id - 1:
 				info.checkpoint = id
