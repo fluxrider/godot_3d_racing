334b4135da461825f7014965959119a17f9bf9ba
fluxrider <doompilot@gmail.com>
2018-12-08 10:57:56 -0500

    simplify checkpoint (still a bit complex)

--- Checkpoints.gd
+++ Checkpoints.gd
@@ -2,7 +2,7 @@ extends Spatial
 
 var lap = 0
 var prev = 3
-var checkpoints = []
+var checkpoint = 0
 
 func _ready():
 	$Area1.connect("body_entered", self, "on_body_entered", [1])
@@ -12,13 +12,13 @@ func _ready():
 func on_body_entered(body, id):
 	if body.name == "Player":
 		if id == 1:
-			if prev == 3 and (lap == 0 or checkpoints == [1,2,3]):
+			if prev == 3 and (lap == 0 or checkpoint == 3):
 				lap += 1
-				checkpoints = [1]
+				checkpoint = 1
 				print("lap: " + str(lap))
 		else:
-			if checkpoints.size() > 0 and checkpoints[-1] == id - 1 and prev == id - 1:
-				checkpoints.append(id)
+			if checkpoint == id - 1 and prev == id - 1:
+				checkpoint = id
 		prev = id
-		print("checkpoint: " + str(id))
-		print("history: " + str(checkpoints))
+		print("id: " + str(id))
+		print("checkpoint: " + str(checkpoint))
