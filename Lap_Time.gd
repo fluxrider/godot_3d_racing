c5231f4c03da93b46e13d3661cf544aad7976bff
fluxrider <doompilot@gmail.com>
2018-12-10 12:35:55 -0500

    (minor) cleanup

--- Lap_Time.gd
+++ Lap_Time.gd
@@ -1,10 +1,5 @@
 extends Label
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	get_parent().find_node("Checkpoints").connect("lap", self, "on_lap")
 	$Timer.connect("timeout", self, "on_timer")
